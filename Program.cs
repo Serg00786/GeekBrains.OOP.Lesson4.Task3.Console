5a659dd93ef907fef71423f446b1b1ec11485d48
Serg <a_s_v86@mail.ru>
2022-01-07 10:59:57 +0300

    Initia;

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains.Lesson4.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator c = new Creator();
+           int a= c.CreateBuild();
+           int b= c.CreateBuild(15);
+            c.DestroyBuilding(a);
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
